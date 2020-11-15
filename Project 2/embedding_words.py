7673d7a0ca7f82ea063467d89d9c0b23bc859f41
xuagram98 <margaux.gerard@student.uclouvain.be>
2020-11-15 13:49:03 +0100

    Keras model on Bert + Color of nodes part 1

--- Project 2/embedding_words.py
+++ Project 2/embedding_words.py
@@ -7,6 +7,7 @@ from copy import deepcopy
 
 import gensim
 import pandas as pd
+import sklearn
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 from nltk import word_tokenize
 from sklearn.naive_bayes import MultinomialNB
@@ -20,7 +21,7 @@ from gensim.parsing.preprocessing import remove_stopwords
 from gensim.parsing.preprocessing import strip_punctuation, strip_non_alphanum
 from sentence_transformers import SentenceTransformer
 import transformers as ppb
-from keras.layers import Conv1D, GlobalMaxPooling1D, Embedding, LSTM
+from keras.layers import Conv1D, GlobalMaxPooling1D, Embedding, LSTM, MaxPooling1D, Flatten
 import nltk
 import matplotlib.pyplot as plt
 import numpy as np
@@ -70,35 +71,21 @@ def bert(df_train, long=False):
         final_df.to_csv('bert.csv')
 
     else:
-        model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
-        df_train = preprocessing_fun(df_train, token = False)
-        listOfEncoding = model.encode(df_train['preprocessed'].tolist(), show_progress_bar = True)
+        model = SentenceTransformer('bert-base-nli-mean-tokens')
+        df_train = preprocessing_fun(df_train, token=False)
+        listOfEncoding = model.encode(df_train['preprocessed'].tolist(), show_progress_bar=True)
         lastLen = len(listOfEncoding[0])
-        print(len(listOfEncoding), len(df_train['subreddit']))
         for i in listOfEncoding:
             if lastLen != len(i):
                 print("Different length :", lastLen, len)
             lastLen = len(i)
         final_df = pd.DataFrame(listOfEncoding)
-        final_df.reset_index(inplace= True)
-        df_train.reset_index(inplace= True)
+        final_df.reset_index(inplace=True)
+        df_train.reset_index(inplace=True)
         final_df['subreddit'] = df_train['subreddit']
-        print(final_df)
-        '''tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/distilbert-base-nli-stsb-mean-tokens")
-        model = AutoModel.from_pretrained("sentence-transformers/distilbert-base-nli-stsb-mean-tokens")
-        #df_train['preprocessed'] = df_train['preprocessed'].apply(lambda x: tokenizer.tokenize(x))
-        tokenized = df_train['preprocessed'].apply(lambda x: tokenizer.encode(x, add_special_tokens=True, truncation=True,
-                                                                               max_length=1000))
-        max_len = 0
-        for i in tokenized.values:
-            if len(i) > max_len:
-                max_len = len(i)
-        padded = np.array([i + [0] * (max_len - len(i)) for i in tokenized.values])
-        attention_mask = np.where(padded != 0, 1, 0)
 
-        final_df = pd.DataFrame(attention_mask)
-        final_df['subreddit'] = df_train['subreddit'].tolist() '''
         print('finished Embedding')
+        final_df.to_csv('bert.csv')
 
     return final_df
 
@@ -107,11 +94,11 @@ def doc2vec_save(df_train):
     documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df_train['preprocessed'].tolist())]
 
     # Define the model
-    doc_model = gensim.models.Doc2Vec( vector_size=40, window=4, compute_loss=True, min_count=10, alpha=0.01)
+    doc_model = gensim.models.Doc2Vec(vector_size=300, window=4, min_count=4, alpha=0.01)
     doc_model.build_vocab(documents)
 
     # Train the model
-    max_epochs = 15
+    max_epochs = 20
     for epoch in range(max_epochs):
         print('iteration {0}'.format(epoch))
         doc_model.train(documents, total_examples=doc_model.corpus_count, epochs=doc_model.iter)
@@ -120,14 +107,14 @@ def doc2vec_save(df_train):
         # fix the learning rate
         doc_model.min_alpha = doc_model.alpha
 
-    doc_model.save("doc2vec.model")
-    print("Model Saved")
-
-    return
+    return doc_model
 
 
-def doc2vec_load(df_test, df_train, nbr_test):
-    doc_model = Doc2Vec.load("doc2vec.model")
+def doc2vec_load(df_test, df_train, nbr_test, model=None):
+    if model is None:
+        doc_model = Doc2Vec.load("doc2vec.model")
+    else:
+        doc_model = model
     dv = doc_model.docvecs.vectors_docs
 
     final_df_tr = pd.DataFrame(dv[:-nbr_test])
@@ -145,8 +132,9 @@ def classifier(df_train, df_test, bert_embedding=True):
         df_train = preprocessing_fun(df_train, token=False)
         df_test = preprocessing_fun(df_test, token=False)
         nbr_test = len(df_test)
-        df_tot = df_train.append(df_test)
-        df_tot = bert(df_tot, long=False)
+        #df_tot = df_train.append(df_test)
+        #df_tot = bert(df_tot, long=False)
+        df_tot = pd.read_csv('bert.csv')
         df_train_encoded = df_tot.iloc[:-nbr_test]
         df_test_encoded = df_tot.iloc[-nbr_test:]
 
@@ -155,38 +143,40 @@ def classifier(df_train, df_test, bert_embedding=True):
         df_test = preprocessing_fun(df_test, token=True)
         nbr_test = len(df_test)
         df_tot = df_train.append(df_test)
-        doc2vec_save(df_tot)
-        df_train_encoded, df_test_encoded = doc2vec_load(df_test, df_train, nbr_test)
+        model = doc2vec_save(df_tot)
+        df_train_encoded, df_test_encoded = doc2vec_load(df_test, df_train, nbr_test, model=model)
 
     min_max_scaler = preprocessing.MinMaxScaler()  # Normalization of data between 0 and 1
-    # selector = VarianceThreshold(threshold=0.01)  # Keep only features with a min variance of 0.01
     X_train = df_train_encoded.loc[:, df_train_encoded.columns != 'subreddit'].to_numpy()
     X_train = min_max_scaler.fit_transform(X_train)
     y_train = df_train_encoded['subreddit']
     X_test = df_test_encoded.loc[:, df_train_encoded.columns != 'subreddit'].to_numpy()
     X_test = min_max_scaler.fit_transform(X_test)
     y_test = df_test_encoded['subreddit']
-
-    inputs = tf.keras.Input(shape=(None,), dtype="int64")
+    X_train_keras = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+    X_test_keras = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
     # Neural network model
     model = tf.keras.Sequential()
-    top_words = 20000
-    embedding_vecor_length = 32
-    model.add(Embedding(top_words, embedding_vecor_length, input_length=40))
-    model.add(LSTM(100))
+    model.add(Conv1D(128, 3, input_shape=X_train_keras.shape[1:3], activation='relu'))
+    model.add(MaxPooling1D(3))
+    model.add(Flatten())
+    model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.Dense(20, activation='relu'))
     model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
-    model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
-    model.fit(X_train, y_train, batch_size=64, epochs=5, shuffle=True)
-    eval = model.evaluate(X_test, y_test)
-    print("Keras model: %.2f%%" % (eval[1] * 100))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(X_train_keras, y_train, batch_size=32, epochs=15, shuffle=True)
+    eval = model.evaluate(X_test_keras, y_test)
+    print("Keras model test: %.2f%%" % (eval[1] * 100))
+    eval = model.evaluate(X_train_keras, y_train)
+    print("Keras model train: %.2f%%" % (eval[1] * 100))
 
     # Classification and prediction
-    models = {"KNN": KNeighborsClassifier(n_neighbors=15, weights='distance'),
+    models = {"KNN": KNeighborsClassifier(n_neighbors=10, weights='distance'),
               "RandomForest": RandomForestClassifier(max_depth=40, random_state=2),
               "Logistic": LogisticRegression(max_iter=2000),
               "NaiveBayes": MultinomialNB(),
-              "GradientBoosting": GradientBoostingClassifier(n_estimators=100, max_depth=20)}
+              "GradientBoosting": GradientBoostingClassifier(n_estimators=10, max_depth=20)}
 
     for name, model in models.items():
         classifier = model.fit(X_train, y_train)
@@ -215,4 +205,4 @@ if __name__ == '__main__':
     df_test = df_test.sample(frac=1).reset_index().drop(columns="index")
     df_train = df_train.sample(frac=1).reset_index().drop(columns="index")
     classifier(df_train, df_test)  # With Bert embedding
-    #classifier(df_train, df_test, bert_embedding=False)  # With Word2Vec embedding
+    #classifier(df_train, df_test, bert_embedding=False)  # With Doc2Vec embedding
--- Project 2/node_embedding.py
+++ Project 2/node_embedding.py
@@ -2,28 +2,22 @@
 #                                           CODE FOR ASSIGNMENT 1                                                     #
 #######################################################################################################################
 import itertools
+
+import community
 import gensim
 from matplotlib import cm
 from nltk.cluster import KMeansClusterer
-from gensim.parsing.preprocessing import preprocess_string
+from gensim.parsing.preprocessing import preprocess_string, strip_short
 from gensim.parsing.preprocessing import strip_tags       # strip html tags
 from gensim.parsing.preprocessing import strip_multiple_whitespaces
-from gensim.parsing.preprocessing import stem_text
-from gensim.parsing.preprocessing import remove_stopwords
 from gensim.parsing.preprocessing import strip_punctuation, strip_non_alphanum
-from sentence_transformers import SentenceTransformer
-from sklearn.datasets import make_classification
 import nltk
+import pandas as pd
+import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-import networkx as nx
-import community
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from joblib.numpy_pickle_utils import xrange
 
 listOfName = ["Pavlovna", "Vasili Kuragin", "Helene", "Pierre", "Hippolyte", "Zherkov", "Captain Timokhin", "Alpatych", "Weyrother",
               "Mortemart", "Morio", "Bolkonskaya", "Nicholas", "Joseph Alexeevich", "Peronskaya", "Vasili Dmitrich", "Tikhon", "Dolgorukov", "Langeron",
@@ -35,17 +29,10 @@ listOfName = ["Pavlovna", "Vasili Kuragin", "Helene", "Pierre", "Hippolyte", "Zh
 
 def build_dict_persons(filename):
     dictForNames = {}
-    dictForNamesTot = {}
-    listOfNameInParagraph = []
-    G = nx.Graph()
     with open(filename) as f:
         while True:
             line = f.readline()
-            if line == "\n":
-                addToGraph(G, listOfNameInParagraph)
-                dictForNames = {}
-                listOfNameInParagraph = []
-            elif line != "":
+            if line != "":
                 # retire les caracteres spéciaux
                 line = castLine(line)
                 for name in listOfName:
@@ -53,13 +40,12 @@ def build_dict_persons(filename):
                     if name == "Count Ilya Rostov":
                         name = name.replace("Ilya", "")
                     if name.lower() in line.lower():
-                        dictForNames[name.lower()] = dictForNames.get(name.lower(), 0) + 1
-                        dictForNamesTot[name.lower()] = dictForNamesTot.get(name.lower(), 0) + 1
-                        if dictForNames[name.lower()] == 1:
-                            listOfNameInParagraph.append(name.lower())
-                            G.add_node(name.lower())
+                        if name.lower() in dictForNames:
+                            dictForNames[name.lower()] += 1
+                        else:
+                            dictForNames[name.lower()] = 1
             else:
-                return dictForNamesTot, G
+                return dictForNames
 
 
 def castLine(line):
@@ -78,22 +64,12 @@ def castLine(line):
     line = line.replace(",", "")
     return line
 
-def addToGraph(G, listName):
-    """
-    An edge was added in G for all pairs of names in listName
-    :param G: represents the graph
-    :param listName: represents the list of all characters
-    """
-    for i in range(len(listName)):
-        for j in range(i+1, len(listName)):
-            G.add_edge(listName[i], listName[j])
-
 
 def preprocessing_as_token(sentences, dict_pers):
     all_sentences = sentences
     # Preprocessing filters
-    filters = [lambda x: x.lower(), stem_text, remove_stopwords, strip_multiple_whitespaces, strip_non_alphanum,
-               strip_punctuation]
+    filters = [lambda x: strip_short(x.lower(), 4), strip_multiple_whitespaces, strip_tags, strip_punctuation,
+               strip_non_alphanum]
     all_sentences_preprocessed = []
     for sent in all_sentences:
         parsed_line = preprocess_string(sent, filters)
@@ -101,12 +77,12 @@ def preprocessing_as_token(sentences, dict_pers):
     return all_sentences_preprocessed
 
 
-def word2vec(filename, dictPerson, show=True):
+def word2vec(filename):
     compound_person_dict = {}
-    for person, count in dictPerson.items():
+    for person, count in build_dict_persons(filename).items():
         person_list = person.split()
         if len(person_list) > 1:
-            compound_person_dict[person] = "".join(person_list)
+            compound_person_dict[person] = "_".join(person_list)
         else:
             compound_person_dict[person] = person
 
@@ -125,10 +101,11 @@ def word2vec(filename, dictPerson, show=True):
             sent_clean = sent_clean.replace(person, compound_person)
         all_sentences_clean2.append(sent_clean)
 
-    preprocessed_sentences = preprocessing_as_token(all_sentences_clean2, dictPerson)
+    preprocessed_sentences = preprocessing_as_token(all_sentences_clean2, build_dict_persons(filename))
 
     # Define the model
-    model = gensim.models.Word2Vec(size=5, window=10, min_count=1, alpha=0.01)
+    model = gensim.models.Word2Vec(size=300, window=30, min_count=1, alpha=0.1)
+    #model = gensim.models.Word2Vec(window=wind, min_count=1)
     model.build_vocab(preprocessed_sentences)
 
     # Train the model
@@ -136,7 +113,6 @@ def word2vec(filename, dictPerson, show=True):
 
     # Get vocabulary from the model
     vocabulary = list(model.wv.vocab)
-    print(f"Vocabulary size :: {len(vocabulary)}")
 
     person_set_processed = [s for s in compound_person_dict.values() if s.lower() in vocabulary]
     person_set_vocab = [s.lower() for s in person_set_processed]
@@ -153,7 +129,7 @@ def word2vec(filename, dictPerson, show=True):
                 edgecolor='k')
     for i, (x, y) in enumerate(V_tranform):
         plt.text(x, y, person_set_vocab[i],
-                 fontsize=12, alpha=0.5)
+                 fontsize=10, alpha=0.5)
     plt.xlabel("t-SNE 1")
     plt.ylabel("t-SNE 2")
     plt.show()
@@ -161,26 +137,211 @@ def word2vec(filename, dictPerson, show=True):
     return person_embedding_vectors, V_tranform, person_set_vocab
 
 
-def clustering(embedding_vectors, V_transform, vocabulary, NUM_CLUSTERS = 4):
+def clustering(embedding_vectors, V_transform, vocabulary, n_clusters=5):
+    NUM_CLUSTERS = n_clusters
     kclusterer_sklearn = KMeans(n_clusters=NUM_CLUSTERS)
 
     # be careful to supply the projected vectors (2D) to the algorithm!
-    assigned_clusters = kclusterer_sklearn.fit_predict(V_transform)
+    assigned_clusters = kclusterer_sklearn.fit_predict(V_tranform)
+
     # DEFINE COLORS OF CLUSTERS
-    colors = cm.nipy_spectral(np.array(assigned_clusters).astype(float) / NUM_CLUSTERS)
+    colors = []
+    for lab in assigned_clusters:
+        if lab == 0:
+            colors.append("cyan")
+        elif lab == 1:
+            colors.append("magenta")
+        elif lab == 2:
+            colors.append("green")
+        elif lab == 3:
+            colors.append("orange")
+        elif lab == 4:
+            colors.append("red")
+        else:
+            colors.append("purple")
 
     # PLOT THE RESULTS
     fig = plt.figure(figsize=(10, 8), dpi=90)
-    plt.scatter(*zip(*V_transform), marker='.', s=50, lw=0, alpha=0.7, c=colors,
+    plt.scatter(*zip(*V_tranform), marker='.', s=100, lw=0, alpha=0.7, c=colors,
                 edgecolor='k')
 
-    for i, (x, y) in enumerate(V_transform):
+    for i, (x, y) in enumerate(V_tranform):
         plt.text(x, y, vocabulary[i], color=colors[i],
-                 fontsize=12, alpha=0.5)
+                 fontsize=10, alpha=0.7)
+
     plt.xlabel("t-SNE 1")
     plt.ylabel("t-SNE 2")
     plt.show()
-    return list(zip(vocabulary, assigned_clusters))
+    return assigned_clusters
+
+
+def addToGraph(G, listName):
+    """
+    An edge was added in G for all pairs of names in listName
+    :param G: represents the graph
+    :param listName: represents the list of all characters
+    """
+    for i in range(len(listName)):
+        for j in range(i+1, len(listName)):
+            G.add_edge(listName[i], listName[j])
+
+
+def buildGraph2():
+    """
+    Builds the graph and links 2 characters if they appear in the same paragraph
+    :return : The builded graph
+    """
+    dictForNames = {}
+    listOfNameInParagraph = []
+    G = nx.Graph()
+    with open("./book.txt") as f:
+        while True:
+            line = f.readline()
+            if line == "\n":
+                addToGraph(G, listOfNameInParagraph)
+                dictForNames = {}
+                listOfNameInParagraph = []
+            elif line != "":
+                # retire les caracteres spéciaux
+                line = castLine(line)
+                for name in listOfName:
+                    # La moitie des apparition sont sous le nom Count Ilya Rostov, l'autre moitie Count Rostov
+                    if name == "Count Ilya Rostov":
+                        name = name.replace("Ilya", "")
+                    if name.lower() in line.lower():
+                        if name.lower() in dictForNames:
+                            dictForNames[name.lower()] += 1
+                        else:
+                            dictForNames[name.lower()] = 1
+                            listOfNameInParagraph.append(name.lower())
+                            G.add_node(name.lower())
+            else:
+                return G
+
+
+def community_layout(g, partition):
+    """
+    Compute the layout for a modular graph.
+
+
+    Arguments:
+    ----------
+    g -- networkx.Graph or networkx.DiGraph instance
+        graph to plot
+
+    partition -- dict mapping int node -> int community
+        graph partitions
+
+
+    Returns:
+    --------
+    pos -- dict mapping int node -> (float x, float y)
+        node positions
+
+    """
+
+    pos_communities = _position_communities(g, partition, scale=3.)
+
+    pos_nodes = _position_nodes(g, partition, scale=1.)
+
+    # combine positions
+    pos = dict()
+    for node in g.nodes():
+        pos[node] = pos_communities[node] + pos_nodes[node]
+
+    return pos
+
+
+def _position_communities(g, partition, **kwargs):
+
+    # create a weighted graph, in which each node corresponds to a community,
+    # and each edge weight to the number of edges between communities
+    between_community_edges = _find_between_community_edges(g, partition)
+
+    communities = set(partition.values())
+    hypergraph = nx.DiGraph()
+    hypergraph.add_nodes_from(communities)
+    for (ci, cj), edges in between_community_edges.items():
+        hypergraph.add_edge(ci, cj, weight=len(edges))
+
+    # find layout for communities
+    pos_communities = nx.spring_layout(hypergraph, **kwargs)
+
+    # set node positions to position of community
+    pos = dict()
+    for node, community in partition.items():
+        pos[node] = pos_communities[community]
+
+    return pos
+
+
+def _find_between_community_edges(g, partition):
+
+    edges = dict()
+
+    for (ni, nj) in g.edges():
+        ci = partition[ni]
+        cj = partition[nj]
+
+        if ci != cj:
+            try:
+                edges[(ci, cj)] += [(ni, nj)]
+            except KeyError:
+                edges[(ci, cj)] = [(ni, nj)]
+
+    return edges
+
+def _position_nodes(g, partition, **kwargs):
+    """
+    Positions nodes within communities.
+    """
+
+    communities = dict()
+    for node, community in partition.items():
+        try:
+            communities[community] += [node]
+        except KeyError:
+            communities[community] = [node]
+
+    pos = dict()
+    for ci, nodes in communities.items():
+        subgraph = g.subgraph(nodes)
+        pos_subgraph = nx.spring_layout(subgraph, **kwargs)
+        pos.update(pos_subgraph)
+
+    return pos
+
+
+def louvain_algo():
+    G = buildGraph2()
+    partitions = {}
+    part = community.best_partition(G)
+    for k, v in part.items():
+        if k in partitions:
+            partitions[k].append(v)
+        else:
+            partitions[k] = [v]
+
+    partition_sorted = dict(sorted(partitions.items()))
+    maxPartition = 0
+    assigned_clusters = []
+    for key, val in partition_sorted.items():
+        maxPartition = max(partition_sorted[key][0], maxPartition)
+        assigned_clusters.append(val[0])
+
+    colors = ["cyan", "magenta", "green", "orange", "red", "purple"]
+    pos = community_layout(G, part)
+    labels = {}
+    for node in G.nodes():
+        labels[node] = node
+    for com in set(part.values()):
+        list_nodes = [nodes for nodes in part.keys() if part[nodes] == com]
+        labs = {k:k for k in list_nodes}
+        nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=colors[com])
+        nx.draw_networkx_labels(G, pos, labs, font_size=9, font_color=colors[com], verticalalignment='bottom',
+                                horizontalalignment='left')
+    plt.show()
+    return partition_sorted, maxPartition
 
 
 def jaccard_similarity(labels1, labels2):
@@ -200,8 +361,9 @@ def jaccard_similarity(labels1, labels2):
     """
     n11 = n10 = n01 = 0
     n = len(labels1)
-    # TODO: Throw exception if len(labels1) != len(labels2)
-    for i, j in itertools.combinations(xrange(n), 2):
+    if len(labels1) != len(labels2):
+        raise Exception()
+    for i, j in itertools.combinations(range(n), 2):
         comembership1 = labels1[i] == labels1[j]
         comembership2 = labels2[i] == labels2[j]
         if comembership1 and comembership2:
@@ -214,20 +376,10 @@ def jaccard_similarity(labels1, labels2):
 
 
 if __name__ == '__main__':
-    book = '../Project 1/book.txt'
-    dict_pers, G = build_dict_persons(book)
-    #print(dict_pers)
-    #sentences = preprocessing_as_token(book)
-    embedding_vectors, V_tranform, person_set_vocab = word2vec(book, dict_pers, show=False)
-    partitionLouvain = community.best_partition(G)
-    maxPartition = 0
-    for i in partitionLouvain:
-        maxPartition = max(partitionLouvain[i], maxPartition)
-    print(maxPartition+1)
-    cluster = clustering(embedding_vectors, V_tranform, person_set_vocab, maxPartition+1)
-    sortedLouvain = sorted(partitionLouvain.items())
-    sortedLouvain = sortedLouvain[:9] + [sortedLouvain[10]] + [sortedLouvain[9]] + sortedLouvain[11:]
-    sortedCluster = sorted(cluster)
-    print(jaccard_similarity([n for (_, n) in sortedLouvain], [n for (_, n) in sortedCluster]))
+    book = './book.txt'
+    partitionLouvain, n_clusters = louvain_algo()
+    embedding_vectors, V_tranform, vocabulary = word2vec(book)
+    assigned_clusters = clustering(embedding_vectors, V_tranform, vocabulary, n_clusters+1)
+    result = jaccard_similarity(assigned_clusters, list(partitionLouvain.values()))
+    print(result)
 
-    #print(jaccard_similarity(cluster, partitionLouvain))
