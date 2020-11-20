2103f42a4dea0f801d6c1f64940e690ae0e205de
xuagram98 <margaux.gerard@student.uclouvain.be>
2020-11-20 21:32:39 +0100

    Code ok mais a relire

--- Projet 3/anonymisation_stress.py
+++ Projet 3/anonymisation_stress.py
@@ -31,13 +31,16 @@ def generalize_data(df):
     """
     new_df = deepcopy(df)
 
-    list_dob = new_df['dob'].tolist()
-    new_list_dob = [list_dob[i].split("/")[1] for i in range(len(list_dob))]
-    new_list_dob = [int(int(new_list_dob[i])//25)*25 for i in range(len(new_list_dob))]
-    new_df['dob'] = new_list_dob
+    new_list_education = []
+    list_education = new_df['education'].tolist()
+    list_employment = new_df['employment'].tolist()
+    for i in range(len(list_education)):
+        if list_employment[i] != 2:
+            new_list_education.append(5)
+        else:
+            new_list_education.append(list_education[i])
 
-    list_zipcode = new_df['zipcode'].tolist()
-    new_df['zipcode'] = [(int(list_zipcode[i])//2000)*2000 for i in range(len(list_zipcode))]
+    new_df['education'] = new_list_education
     return new_df
 
 
@@ -57,17 +60,17 @@ def features_encoding(df):
 
 def form_classes(df, flag=True):
     if flag:
-        sub_df = df[["dob", "zipcode"]]
+        sub_df = df[["employment", "children", "marital_status", "commute_time", "education"]]
         list_tuples = sub_df.to_records()  # Rows of the sub-dataframe into tuples
         dict_classes = dict()
-        for (index, dob, zipcode) in list_tuples:
-            dict_classes[(dob, zipcode)] = dict_classes.get((dob, zipcode), 0) + 1
+        for (index, employment, children, marital, commute_time, education) in list_tuples:
+            dict_classes[(employment, children, marital, commute_time, education)] = dict_classes.get((employment, children, marital, commute_time, education), 0) + 1
     else:
-        sub_df = df[["dob", "zipcode", "disease"]]
+        sub_df = df[["employment", "children", "marital_status", "commute_time", "education", "disease"]]
         list_tuples = sub_df.to_records()  # Rows of the sub-dataframe into tuples
         dict_classes = dict()
-        for (index, dob, zipcode, disease) in list_tuples:
-            dict_classes[(dob, zipcode, disease)] = dict_classes.get((dob, zipcode, disease), 0) + 1
+        for (index, employment, children, marital, commute_time, education, disease) in list_tuples:
+            dict_classes[(employment, children, marital, commute_time, education, disease)] = dict_classes.get((employment, children, marital, commute_time, education, disease), 0) + 1
 
     dict_classes = dict(sorted(dict_classes.items(), key=lambda x: x[1], reverse=False))
     return dict_classes
@@ -86,7 +89,9 @@ def k_anonymization(df, k=2):
         if val >= k:
             continue
         else:
-            index = new_df.index[(new_df["dob"] == key[0]) & (new_df["zipcode"] == key[1])].tolist()
+            index = new_df.index[(new_df["employment"] == key[0]) & (new_df["children"] == key[1]) &
+                                 (new_df["marital_status"] == key[2]) & (new_df["commute_time"] == key[3]) &
+                                 (new_df["education"] == key[4])].tolist()
             index_to_drop += index
 
     new_df = new_df.drop(index_to_drop, axis=0)
@@ -99,15 +104,17 @@ def l_diversity(df, l=2):
     dict_diversity = {}
     new_df = deepcopy(df)
     for key, val in dict_clas.items():
-        current_dict = dict_diversity.get(key[0:2], {})
-        current_dict[key[2]] = current_dict.get(key[2], 0) + 1
-        dict_diversity[key[0:2]] = current_dict
+        current_dict = dict_diversity.get(key[0:5], {})
+        current_dict[key[5]] = current_dict.get(key[5], 0) + 1
+        dict_diversity[key[0:5]] = current_dict
 
     for key, val in dict_diversity.items():
         if len(val) >= l:
             continue
         else:
-            index = new_df.index[(new_df["dob"] == key[0]) & (new_df["zipcode"] == key[1])].tolist()
+            index = new_df.index[(new_df["employment"] == key[0]) & (new_df["children"] == key[1]) &
+                                 (new_df["marital_status"] == key[2]) & (new_df["commute_time"] == key[3])
+                                 & (new_df["education"] == key[4])].tolist()
             index_to_drop += index
 
     new_df = new_df.drop(index_to_drop, axis=0)
@@ -137,11 +144,10 @@ if __name__ == '__main__':
     hashed_df = remove_direct_identifier(encoded_df)
     pseudonimized_df = generalize_data(hashed_df)
     anonymised_df = k_anonymization(pseudonimized_df, k=10)
-    anonymised_df = anonymised_df.drop(["gender", "number_vehicles", "education", "children", "marital_status",
-                                        "employment", "commute_time", "accommodation"], axis=1)
-    divsersified_df = l_diversity(anonymised_df, l=8)
+    anonymised_df = anonymised_df.drop(["gender", "zipcode", "ancestry", "number_vehicles", "accommodation"], axis=1)
+    divsersified_df = l_diversity(anonymised_df, l=10)
     print(f"Loss between original and pseudominized : {loss_entropy(original_df, pseudonimized_df)}")
     print(f"Loss between pseudominized and anonymized : {loss_entropy(pseudonimized_df, anonymised_df)}")
     print(f"Loss between anonymized and divsersified : {loss_entropy(anonymised_df, divsersified_df)}")
 
-    anonymised_df.to_csv('test2.csv')
+    divsersified_df.to_csv('test2.csv')
